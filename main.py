25588f1dd2d3f24e7cb070931eb0f6a53e55f6af
VIDDESLAPTOP\Vidar <vidar.halmstad@hotmail.com>
2018-08-19 13:30:27 +0200

    Added base pygame code.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+
+def main ():
+    pygame.init()
+
+    pygame.display.set_caption("2d platform game it")
+    screen = pygame.display.set_mode((1600, 900))
+    running = True
+
+    while (running):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+main();
