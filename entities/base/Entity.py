21c628d7880c0d0f0a3c7e38d72cf3475f18e579
VIDDESLAPTOP\Vidar <vidar.halmstad@hotmail.com>
2018-08-19 15:08:41 +0200

    Entity loads a sprite by name.

--- entities/base/Entity.py
+++ entities/base/Entity.py
@@ -1,9 +1,15 @@
+import os
+import pygame
+
+
 class Entity:
 
     def __init__(self, x, y, name):
         self.spawnX = x
         self.spawnY = y
         self.name = name
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../" + name + "/" + name + ".png")
+        self.sprite = pygame.image.load(path)
 
     def update(self, deltaTime, state):
         self.y -= state.gravity * deltaTime;
