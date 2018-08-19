e92e3b1656d278182c377e4d80d2c853991e7550
VIDDESLAPTOP\Vidar <vidar.halmstad@hotmail.com>
2018-08-19 16:32:29 +0200

    Add list of tiles.

--- src/main.py
+++ src/main.py
@@ -10,12 +10,13 @@ def main ():
     pygame.display.set_caption("2d platform game it")
     screen = pygame.display.set_mode((1600, 900))
     running = True
+    tiles = [Tile(500, 500, "Grass")]
+    tiles.append(Tile(300, 300, "Grass"))
 
     DATA = LevelData(98.2)
     lastFrameTicks = 0
 
     player = Character(200, 200, "character", 10)
-    grassTile1 = Tile(500, 500, "Grass")
 
     while (running):
         screen.fill((100, 100, 100))
@@ -29,7 +30,8 @@ def main ():
         lastFrameTicks = t
         player.update(deltaTime, DATA)
         player.draw(screen)
-        grassTile1.draw(screen)
+        for tile in tiles:
+            tile.draw(screen)
         pygame.display.update()
 
-main();
+main()
