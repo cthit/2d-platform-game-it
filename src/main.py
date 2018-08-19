0e6ed62fce3c86b8cbbcc0ce24d6430c47fda936
VIDDESLAPTOP\Vidar <vidar.halmstad@hotmail.com>
2018-08-19 14:25:48 +0200

    now calculates deltaTime in main.

--- src/main.py
+++ src/main.py
@@ -7,9 +7,14 @@ def main ():
     screen = pygame.display.set_mode((1600, 900))
     running = True
 
+    lastFrameTicks = 0
     while (running):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        # Calculate deltaTime 
+        t = pygame.time.get_ticks()
+        deltaTime = (t - lastFrameTicks) / 1000.0
+        lastFrameTicks = t
 main();
