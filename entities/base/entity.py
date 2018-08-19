fc777e50e389d3df09c3c72fe7b2db2d0053d90b
VIDDESLAPTOP\Vidar <vidar.halmstad@hotmail.com>
2018-08-19 14:25:38 +0200

    Added entity class

---
+++ entities/base/entity.py
@@ -0,0 +1,10 @@
+class entity:
+
+    def __init__(self, spawnX, spawnY, name):
+        self.spawnX = spawnX
+        self.spawnY = spawnY
+        self.name = name
+
+    def update(deltaTime):
+        return
+
