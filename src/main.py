e25ac898e64f39f619b400d21933daa542269186
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-08-19 21:36:52 +0200

    Add tile_prototypes list and fills the bottom of the screen with grass.

--- src/main.py
+++ src/main.py
@@ -4,14 +4,26 @@ from entities.character.Character import Character
 from src.LevelData import LevelData
 from tiles.base.Tile import Tile
 
+def getTilePrototypes():
+    tilePrototypes = [Tile(0, 0, "Grass")]
+    return tilePrototypes
+
+# Spawns a copy of a tileprototype and returns the new one.
+def spawnTile(tile, x, y):
+    newTile = Tile(x, y, tile.name)
+    return newTile
 
 def main ():
     pygame.init()
     pygame.display.set_caption("2d platform game it")
     screen = pygame.display.set_mode((1600, 900))
     running = True
-    tiles = [Tile(500, 500, "Grass")]
-    tiles.append(Tile(300, 300, "Grass"))
+    tile_prototypes = getTilePrototypes()
+    tiles = []
+    for i in range(0, screen.get_width(), 21):
+        # Spawns a floor of grass at the bottom of the screen.
+        tiles.append(spawnTile(tile_prototypes[0], i, (screen.get_height() - 21)))
+
 
     DATA = LevelData(98.2)
     lastFrameTicks = 0
