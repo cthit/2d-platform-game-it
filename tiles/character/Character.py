491d6778f6c4c50f7fc3afe50522a9ff3ca11504
VIDDESLAPTOP\Vidar <vidar.halmstad@hotmail.com>
2018-08-19 14:39:43 +0200

    Added character class

---
+++ tiles/character/Character.py
@@ -0,0 +1,10 @@
+from entities.base.Entity import Entity
+
+class Character(Entity):
+
+    def __init__(self, x, y, name, movementSpeed):
+        Entity.__init__(self, x, y, name)
+        self.movementSpeed = movementSpeed
+
+    def update(self, deltaTime, state):
+        return
