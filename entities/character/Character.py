86fd8d60e55e3c144e4d879cbfdf8c79760904fc
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-08-20 21:50:03 +0200

    Adds acceleration and "onFloor" variables to character.

--- entities/character/Character.py
+++ entities/character/Character.py
@@ -5,6 +5,8 @@ class Character(Entity):
     def __init__(self, x, y, name, movement_speed = 10):
         Entity.__init__(self, x, y, name)
         self.movementSpeed = movement_speed
+        self.acceleration = (0, 0)
+        self.onFloor = False
 
     def update(self, deltaTime, state):
         self.y = self.y + (deltaTime * state.gravity)
