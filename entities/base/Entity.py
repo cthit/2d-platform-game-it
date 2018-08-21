d01e757172a176d12d9d0df5d161a7c44a8cd733
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-08-21 14:24:10 +0200

    Moves gravity code from character to entity.

--- entities/base/Entity.py
+++ entities/base/Entity.py
@@ -10,6 +10,8 @@ class Entity:
         self.width = 1
         self.height = 1
         self.name = name
+        self.velocity = pygame.math.Vector2(0, 0)
+        self.onFloor = False
         path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../" + name + "/" + name + ".png")
         try:
             self.sprite = pygame.image.load(path)
@@ -17,7 +19,11 @@ class Entity:
             print("Could not load sprite for " + name)
 
     def update(self, deltaTime, state):
-        self.y -= state.gravity * deltaTime;
+        if not self.onFloor:
+            self.velocity.y = (self.velocity.y + state.gravity * deltaTime)
+
+        self.y = self.y + self.velocity.y
+        self.x = self.x + self.velocity.x
 
     def draw(self, screen):
         screen.blit(self.sprite, (self.x, self.y))
--- entities/character/Character.py
+++ entities/character/Character.py
@@ -6,12 +6,3 @@ class Character(Entity):
     def __init__(self, x, y, name, movement_speed = 10):
         Entity.__init__(self, x, y, name)
         self.movementSpeed = movement_speed
-        self.velocity = pygame.math.Vector2(0, 0)
-        self.onFloor = False
-
-    def update(self, deltaTime, state):
-        if not self.onFloor:
-            self.velocity.y = (self.velocity.y + state.gravity * deltaTime)
-
-        self.y = self.y + self.velocity.y
-        self.x = self.x + self.velocity.x
