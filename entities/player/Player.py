80726c2b2fcd98383a2a369206d0a0f1a8c45b76
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-08-23 14:52:02 +0200

    Adds update method to player.

--- entities/player/Player.py
+++ entities/player/Player.py
@@ -4,3 +4,7 @@ from entities.character.Character import Character
 class Player(Character):
     def __init__(self, x, y, name):
         super().__init__(x, y, name, 10)
+
+    def update(self, deltaTime, state):
+        super().update(self, deltaTime, state)
+
