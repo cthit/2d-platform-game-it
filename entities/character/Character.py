1ffdcf46b353eadecd0afd5348b78df8fcbc4010
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-08-23 14:56:48 +0200

    Adds back default value to Character.movementspeed to avoid level load error.

--- entities/character/Character.py
+++ entities/character/Character.py
@@ -3,6 +3,6 @@ import pygame
 
 class Character(Entity):
 
-    def __init__(self, x, y, name, movement_speed):
+    def __init__(self, x, y, name, movement_speed = 10):
         Entity.__init__(self, x, y, name)
         self.movementSpeed = movement_speed
