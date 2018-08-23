cd937c96958d21731485fbcbe5f7d506e50eb2ee
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-08-23 14:50:01 +0200

    Sets players movementspeed to 10

--- entities/player/Player.py
+++ entities/player/Player.py
@@ -3,4 +3,4 @@ from entities.character.Character import Character
 
 class Player(Character):
     def __init__(self, x, y, name):
-        super().__init__(x, y, name)
+        super().__init__(x, y, name, 10)
