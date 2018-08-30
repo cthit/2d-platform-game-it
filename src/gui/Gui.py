bc01cd6bc806880e930842fb6634db1dd394d452
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-08-30 20:45:25 +0200

    Began work on Gui.update

--- src/gui/Gui.py
+++ src/gui/Gui.py
@@ -2,4 +2,13 @@ import pygame
 
 class Gui:
     def __init__(self):
-        pass
+        self.gui_elements = list()
+
+    def update(self, mouse, events):
+        for element in self.gui_elements:
+            if element.contains(mouse.get_pos):
+                element.on_hover()
+
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
