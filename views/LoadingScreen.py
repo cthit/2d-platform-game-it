189723f3fb87b474f121deea6a6d511ba8a22aff
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-01 18:54:43 +0200

    Add image to loadingscreen.

--- views/LoadingScreen.py
+++ views/LoadingScreen.py
@@ -10,4 +10,5 @@ def load_view(gui, game):
     gui.add_gui_element(TextBlock("Loading next level", 200, 200))
     path = os.path.dirname(os.path.realpath(__file__)) + "/../resources/funtique/FuntiqueGUI_0001_Loader.png"
     loading_image = pygame.image.load(path)
-    gui.add_gui_element(Image(200, 250, loading_image))
+    loading_image = pygame.transform.scale(loading_image, (40, 40))
+    gui.add_gui_element(Image(300, 300, 1, 1, loading_image))
