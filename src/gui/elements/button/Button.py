4046a14bafea891c7b77cb449787e33a3a0d34f3
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-06 19:34:37 +0200

    Adds possibility to resize images.

--- src/gui/elements/button/Button.py
+++ src/gui/elements/button/Button.py
@@ -19,20 +19,34 @@ class Button(GuiElement):
         self.text_color = text_color
         self.text_surface = font.render(text, True, text_color)
         self.callback = callback
+        self.size_multiplier = 1
 
     def draw(self, surface):
         super(Button, self).draw(surface)
         (tcx, tcy, icx, icy) = self.text_surface.get_rect().center + self.image.get_rect().center
         surface.blit(self.text_surface, (self.x + icx - tcx, self.y + icy - tcy))
 
+    def resize_button_to_width(self, new_width):
+        self.size_multiplier = new_width / self.image.get_rect().width
+        self.change_image(self.image)
+
+    def resize_button_to_height(self, new_height):
+        self.size_multiplier = new_height / self.image.get_rect().height
+        self.change_image(self.image)
+
     def on_hover(self):
-        self.image = button_images["light"]
+        self.change_image(button_images["light"])
 
     def on_mouse_down(self):
-        self.image = button_images["dark"]
+        self.change_image(button_images["dark"])
 
     def on_mouse_up(self):
         self.callback()
 
     def on_mouse_leave(self):
-        self.image = button_images["green"]
+        self.change_image(button_images["green"])
+
+    def change_image(self, image):
+        new_width = int(image.get_rect().width * self.size_multiplier)
+        new_height = int(image.get_rect().height * self.size_multiplier)
+        self.image = pygame.transform.scale(image, (new_width, new_height))
