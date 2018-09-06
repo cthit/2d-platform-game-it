1fb3c8a399d7c5c682d6b6c3823f131ffd090cc6
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-06 19:43:08 +0200

    Fix #25

--- views/MainMenu.py
+++ views/MainMenu.py
@@ -4,5 +4,20 @@ from src.gui.elements.text.TextBlock import TextBlock
 
 
 def load_view(gui, game_methods: GameMethods):
-    gui.add_gui_element(TextBlock("This is a text block!", 100, 100))
-    gui.add_gui_element(Button("Start Game", 100, 150, lambda: game_methods.load_level_by_index(1)))
+    button_one = Button("Level One", 300, 50, lambda: game_methods.load_level_by_index(1))
+    button_one.resize_button_to_height(50)
+
+    button_two = Button("Level Two", 300, 150, lambda: game_methods.load_level_by_index(2))
+    button_two.resize_button_to_height(50)
+
+    button_three = Button("Level Three", 300, 250, lambda: game_methods.load_level_by_index(3))
+    button_three.resize_button_to_height(50)
+
+    button_four = Button("Level Four", 300, 350, lambda: game_methods.load_level_by_index(4))
+    button_four.resize_button_to_height(50)
+
+    gui.add_gui_element(button_one)
+    gui.add_gui_element(button_two)
+    gui.add_gui_element(button_three)
+    gui.add_gui_element(button_four)
+
