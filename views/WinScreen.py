ed24570f1348bd9e1ac6f0ba05abc5df7f75c7af
Vidar Magnusson <vidar.halmstad@hotmail.com>
2018-09-06 18:41:54 +0200

    Renamed EndScreen to WinScreen, also disabled large_level_test.

--- views/WinScreen.py
+++ views/WinScreen.py
@@ -4,5 +4,5 @@ from src.gui.elements.text.TextBlock import TextBlock
 
 
 def load_view(gui, game_methods: GameMethods):
-    gui.add_gui_element(TextBlock("You Win!", 200, 200))
-    gui.add_gui_element(Button("Restart Game", 300, 100, lambda: game_methods.load_level_by_index(1)))
+    gui.add_gui_element(TextBlock("You Win!", 300, 100))
+    gui.add_gui_element(Button("Restart Game", 200, 200, lambda: game_methods.load_level_by_index(1)))
